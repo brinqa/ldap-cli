331fd618b9df63e5a34912c0eeecf29435a23acb
Will Droste <will.droste@brinqa.com>
2020-01-27 12:06:02 -0600

    Add TSL/SSL params

--- src/main/kotlin/brinqa/ldap/cli/App.kt
+++ src/main/kotlin/brinqa/ldap/cli/App.kt
@@ -11,13 +11,19 @@ import org.forgerock.opendj.ldap.ByteString
 import org.forgerock.opendj.ldap.DecodeOptions
 import org.forgerock.opendj.ldap.ErrorResultException
 import org.forgerock.opendj.ldap.LDAPConnectionFactory
+import org.forgerock.opendj.ldap.LDAPOptions
+import org.forgerock.opendj.ldap.SSLContextBuilder
 import org.forgerock.opendj.ldap.SearchResultHandler
 import org.forgerock.opendj.ldap.SearchScope
+import org.forgerock.opendj.ldap.TrustManagers
 import org.forgerock.opendj.ldap.controls.SimplePagedResultsControl
 import org.forgerock.opendj.ldap.requests.Requests
 import org.forgerock.opendj.ldap.responses.Result
 import org.forgerock.opendj.ldap.responses.SearchResultEntry
 import org.forgerock.opendj.ldap.responses.SearchResultReference
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport
+import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder
+import java.io.IOException
 
 class App : CliktCommand() {
     override fun run() {
@@ -25,10 +31,15 @@ class App : CliktCommand() {
 }
 
 class LDAPSearch : CliktCommand(name = "search", help = "Search LDAP Directory") {
+
     private val host: String by option(help = "Hostname for the AD server").prompt("Hostname")
     private val port: Int by option(help = "Port for the AD server").int().default(636)
     private val username: String by option(help = "Username for the connection").default("")
     private val password: String by option(help = "Password for the connection").default("")
+
+    private val tls: String by option(help = "Enable TLS").default("false")
+    private val ssl: String by option(help = "Enable SSL").default("false")
+
 
     private val baseContext: String by option(help = "Base context for search.").required()
 
@@ -37,7 +48,7 @@ class LDAPSearch : CliktCommand(name = "search", help = "Search LDAP Directory")
 
 
     override fun run() {
-        val factory = LDAPConnectionFactory(host, port)
+        val factory = LDAPConnectionFactory(host, port, buildLDAPOptions())
         factory.connection.use { conn ->
             if (username.isNotBlank()) {
                 conn.bind(username, password.toCharArray())
@@ -73,8 +84,30 @@ class LDAPSearch : CliktCommand(name = "search", help = "Search LDAP Directory")
                 cookie = control.cookie
             } while (cookie.length() != 0)
         }
-
     }
+
+    private fun buildLDAPOptions(): LDAPOptions {
+        val lo = LDAPOptions()
+        //lo.setProviderClassLoader(getClass().getClassLoader());
+        lo.tcpnioTransport = buildTransportInstance()
+        if (ssl.toBoolean() || tls.toBoolean()) {
+            val bld = SSLContextBuilder().setTrustManager(TrustManagers.trustAll())
+            val sslContext = bld.sslContext
+            lo.sslContext = sslContext
+            lo.setUseStartTLS(tls.toBoolean())
+        }
+        return lo
+    }
+
+    private fun buildTransportInstance(): TCPNIOTransport = try {
+        val builder = TCPNIOTransportBuilder.newInstance()
+        val transport = builder.build()
+        transport.start()
+        transport
+    } catch (e: IOException) {
+        throw RuntimeException(e)
+    }
+
 }
 
 fun main(args: Array<String>) = App()
