b7e5f00c7b83d95212745105d13a16408a2d9f5f
Will Droste <will.droste@brinqa.com>
2022-06-13 10:58:19 -0500

    Add support for schema

--- src/main/kotlin/brinqa/ldap/cli/App.kt
+++ src/main/kotlin/brinqa/ldap/cli/App.kt
@@ -8,6 +8,7 @@ import com.github.ajalt.clikt.parameters.options.prompt
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.int
 import org.forgerock.opendj.ldap.ByteString
+import org.forgerock.opendj.ldap.DN
 import org.forgerock.opendj.ldap.DecodeOptions
 import org.forgerock.opendj.ldap.ErrorResultException
 import org.forgerock.opendj.ldap.LDAPConnectionFactory
@@ -21,6 +22,8 @@ import org.forgerock.opendj.ldap.requests.Requests
 import org.forgerock.opendj.ldap.responses.Result
 import org.forgerock.opendj.ldap.responses.SearchResultEntry
 import org.forgerock.opendj.ldap.responses.SearchResultReference
+import org.forgerock.opendj.ldap.schema.AttributeType
+import org.forgerock.opendj.ldap.schema.Schema
 import org.glassfish.grizzly.nio.transport.TCPNIOTransport
 import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder
 import java.io.IOException
@@ -32,11 +35,13 @@ import javax.net.ssl.SSLSocketFactory
 
 
 class App : CliktCommand() {
+
     override fun run() {
     }
 }
 
 class PrintSupportedCiphers : CliktCommand(name = "ciphers") {
+
     override fun run() {
         val ssf = SSLServerSocketFactory.getDefault() as SSLServerSocketFactory
 
@@ -59,7 +64,8 @@ class PrintSupportedCiphers : CliktCommand(name = "ciphers") {
     }
 }
 
-class KeyStoreDebug : CliktCommand(name = "keystore", help = "check the keystore") {
+class KeyStoreDebug : CliktCommand(name = "ssl-connect", help = "check the keystore") {
+
     private val host: String by option(help = "Hostname for the AD server").default("google.com")
     private val port: Int by option(help = "Port for the AD server").int().default(443)
 
@@ -75,6 +81,52 @@ class KeyStoreDebug : CliktCommand(name = "keystore", help = "check the keystore
             print(inputStream.read())
         }
         println("Secured connection performed successfully")
+    }
+}
+
+class LDAPSchema : CliktCommand(name = "schema", help = "Print LDAP Schema") {
+
+    private val host: String by option(help = "Hostname for the AD server").prompt("Hostname")
+    private val port: Int by option(help = "Port for the AD server").int().default(636)
+    private val username: String by option(help = "Username for the connection").default("")
+    private val password: String by option(help = "Password for the connection").default("")
+
+    private val tls: String by option(help = "Enable TLS").default("false")
+    private val ssl: String by option(help = "Enable SSL").default("false")
+
+    private val protocol: String by option(help = "Protocol").default("TLSv1.2")
+
+    private val baseContext: String by option(help = "Base context for search.").required()
+
+    override fun run() {
+        val factory = buildFactory(host, port, protocol, ssl, tls)
+        factory.connection.use { conn ->
+            if (username.isNotBlank()) {
+                conn.bind(username, password.toCharArray())
+            }
+            println("building schema from context: [$baseContext]")
+
+            val schema = Schema.readSchema(conn, DN.rootDN())
+            schema.objectClasses.forEach { oc ->
+                println("ObjectClass: $oc.nameOrOID")
+
+                println("Required Attributes:")
+                oc.requiredAttributes.forEach(this::printAttributeType)
+
+                println("Optional Attributes:")
+                oc.optionalAttributes.forEach(this::printAttributeType)
+
+                println("Declared Required Attributes:")
+                oc.declaredRequiredAttributes.forEach(this::printAttributeType)
+
+                println("Declared Optional Attributes:")
+                oc.declaredOptionalAttributes.forEach(this::printAttributeType)
+            }
+        }
+    }
+
+    private fun printAttributeType(attrType: AttributeType) {
+        println("Attribute OID: ${attrType.oid}\tName(s): ${attrType.names}\t Description: ${attrType.description}")
     }
 }
 
@@ -96,7 +148,7 @@ class LDAPSearch : CliktCommand(name = "search", help = "Search LDAP Directory")
     private val filter: String by option(help = "Filter for search.").default("")
 
     override fun run() {
-        val factory = LDAPConnectionFactory(host, port, buildLDAPOptions(protocol))
+        val factory = buildFactory(host, port, protocol, ssl, tls)
         factory.connection.use { conn ->
             if (username.isNotBlank()) {
                 conn.bind(username, password.toCharArray())
@@ -134,28 +186,33 @@ class LDAPSearch : CliktCommand(name = "search", help = "Search LDAP Directory")
         }
     }
 
-    private fun buildLDAPOptions(protocol: String): LDAPOptions {
-        val lo = LDAPOptions()
-        //lo.setProviderClassLoader(getClass().getClassLoader());
-        lo.tcpnioTransport = buildTransportInstance()
-        if (ssl.toBoolean() || tls.toBoolean()) {
-            println("Using SSL: ${ssl.toBoolean()}")
-            lo.sslContext = sslContext(protocol)
-            println("Using TLS: ${tls.toBoolean()}")
-            lo.setUseStartTLS(tls.toBoolean())
-        }
-        return lo
-    }
 
-    private fun buildTransportInstance(): TCPNIOTransport = try {
-        val builder = TCPNIOTransportBuilder.newInstance()
-        val transport = builder.build()
-        transport.start()
-        transport
-    } catch (e: IOException) {
-        throw RuntimeException(e)
-    }
+}
 
+fun buildFactory(host: String, port: Int, protocol: String, ssl: String, tls: String): LDAPConnectionFactory {
+    return LDAPConnectionFactory(host, port, buildLDAPOptions(protocol, ssl, tls))
+}
+
+private fun buildLDAPOptions(protocol: String, ssl: String, tls: String): LDAPOptions {
+    val lo = LDAPOptions()
+    //lo.setProviderClassLoader(getClass().getClassLoader());
+    lo.tcpnioTransport = buildTransportInstance()
+    if (ssl.toBoolean() || tls.toBoolean()) {
+        println("Using SSL: ${ssl.toBoolean()}")
+        lo.sslContext = sslContext(protocol)
+        println("Using TLS: ${tls.toBoolean()}")
+        lo.setUseStartTLS(tls.toBoolean())
+    }
+    return lo
+}
+
+private fun buildTransportInstance(): TCPNIOTransport = try {
+    val builder = TCPNIOTransportBuilder.newInstance()
+    val transport = builder.build()
+    transport.start()
+    transport
+} catch (e: IOException) {
+    throw RuntimeException(e)
 }
 
 fun sslContext(protocol: String): SSLContext {
